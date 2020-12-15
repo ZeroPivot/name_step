8ded69435758b1a1d213808acf33ee12dfe996f3
Duke Grable <zerothpivot@gmail.com>
2020-12-15 07:16:14 -0800

    delete this

--- lib/magicwand.rb
+++ lib/magicwand.rb
@@ -6,3 +6,22 @@ end
 def string_split(string:, split_by: "")
     string.split(split_by)
 end
+
+def compute_word_steps!(goal_string:)
+
+    steps = 0
+    begin_string = 'a' * string_arity(goal_string)     
+    compute_string = string_split(string: goal_string) #array of chars
+
+    compute_string.reverse.each_with_index do |value, index|
+        char = 'a' #start at the beginning of the alphabet
+        count_distance = 0
+        until (char == value)
+            count_distance += 1
+            char.succ!       
+        end
+        steps += (26 ** index) * count_distance #sum formula to calculate steps without a long loop
+                
+    end
+    steps
+end
--- main_word_diff.rb
+++ main_word_diff.rb
@@ -13,26 +13,6 @@ goal_string2 = gets.chomp
 puts
 puts 
 
-def compute_word_steps!(goal_string:)
-
-    steps = 0
-    begin_string = 'a' * string_arity(goal_string)     
-    compute_string = string_split(string: goal_string) #array of chars
-
-    compute_string.reverse.each_with_index do |value, index|
-        char = 'a' #start at the beginning of the alphabet
-        count_distance = 0
-        until (char == value)
-            count_distance += 1
-            char.succ!       
-        end
-        steps += (26 ** index) * count_distance #sum formula to calculate steps without a long loop
-                
-    end
-    steps
-end
-
-
 
 steps_first = compute_word_steps!(goal_string: goal_string)
 steps_second = compute_word_steps!(goal_string: goal_string2)
